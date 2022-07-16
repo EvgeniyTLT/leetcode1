996c141996fe72ed365d2a3a82af84bc533cd299
EvgeniyTLT <Toy444@ya.ru>
2022-07-16 21:54:46 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Введите через пробел '(', ')', '{', '}', '[', ']'");
-        Boolean result = true;
+        int counter = 0;
+        boolean result = true;
         String s1 = "(";
         String s2 = ")";
         String s3 = "{";
@@ -14,31 +14,49 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         String[] array = s.split(" ");
-        for (String u : array
-        ) {
+        for (String u : array) {
             if (!u.equals(s1) && !u.equals(s2) && !u.equals(s3) && !u.equals(s4) && !u.equals(s5) && !u.equals(s6)) {
                 System.out.println("направильный ввод");
-                result = false;
+                return;
             }
         }
-        if (result) {
-            System.out.println("Вы ввели: " + s);
-            for (int i = 0; i < array.length - 1; i++) {
-                if (array[i].equals(s1)) {
-                    if (!array[i + 1].equals(s2)) {
-                        result = false;
-                    }
-                } else if (array[i].equals(s3)) {
-                    if (!array[i + 1].equals(s4)) {
-                        result = false;
-                    }
-                } else if (array[i].equals(s5)) {
-                    if (!array[i + 1].equals(s6)) {
-                        result = false;
-                    }
+        if (array.length % 2 != 0) {
+            System.out.println("Вы ввели нечетное число");
+            return;
+        }
+
+        while (result && counter < array.length) {
+            System.out.println("еще крутит");
+            if (array[counter].equals(s1)) {
+                if (!array[counter + 1].equals(s2)) {
+                    result = false;
+                }
+            } else if (array[counter].equals(s3)) {
+                if (!array[counter + 1].equals(s4)) {
+                    result = false;
+                }
+            } else if (array[counter].equals(s5)) {
+                if (!array[counter + 1].equals(s6)) {
+                    result = false;
+                }
+            } else if (array[counter].equals(s2)) {
+                if (!array[counter + 1].equals(s1)) {
+                    result = false;
+                }
+            } else if (array[counter].equals(s4)) {
+                if (!array[counter + 1].equals(s3)) {
+                    result = false;
+                }
+            } else if (array[counter].equals(s6)) {
+                if (!array[counter + 1].equals(s5)) {
+                    result = false;
                 }
             }
-            System.out.println(result);
+            System.out.println("значение в этом круге-" + counter + " - " + result);
+            counter += 2;
+            System.out.println("будут такие значение в следующем круге-" + counter + " - " + result);
         }
+        System.out.println("результат: " + result);
     }
 }
+
